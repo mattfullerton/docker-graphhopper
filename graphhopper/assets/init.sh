78f32d5d174dab0783f98bc03d2666bdaa446cd1
Dustin Williams <dusty_williams825@live.com>
2015-02-19 22:11:14 -0500

    Auto Download North Carolina

--- graphhopper/assets/init.sh
+++ graphhopper/assets/init.sh
@@ -12,5 +12,9 @@ rm *.zip
 mv /tmp/config.properties $GRAPHHOPPER_DIR
 mv /tmp/start.sh $GRAPHHOPPER_DIR
 
+mkdir -p ~/private/graphhopper-data/berlin/
+cd ~/private/graphhopper-data/berlin/
+wget http://download.geofabrik.de/north-america/us/north-carolina-latest.osm.pbf
+
 echo "Showing contents of ${GRAPHHOPPER_DIR}"
 ls $GRAPHHOPPER_DIR
