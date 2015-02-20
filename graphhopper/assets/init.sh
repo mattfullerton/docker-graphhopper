056779e448f31aab8ceed6337974dc1136d666cf
Dustin Williams <dusty_williams825@live.com>
2015-02-20 02:52:03 -0500

    Update init.sh

--- graphhopper/assets/init.sh
+++ graphhopper/assets/init.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Create variable for Graph Hopper Directory
 GRAPHHOPPER_DIR=/graphhopper
 
+# Make the directory, then Change directory
 mkdir $GRAPHHOPPER_DIR
 cd $GRAPHHOPPER_DIR
 
+# Get .zip file from web, then unzip the .zip file, then remove (delete) the .zip file
 wget https://oss.sonatype.org/content/groups/public/com/graphhopper/graphhopper-web/0.3/graphhopper-web-0.3-bin.zip
 unzip *.zip
 rm *.zip
@@ -12,8 +15,9 @@ rm *.zip
 mv /tmp/config.properties $GRAPHHOPPER_DIR
 mv /tmp/start.sh $GRAPHHOPPER_DIR
 
-mkdir -p ~/private/graphhopper-data/berlin/
-cd ~/private/graphhopper-data/berlin/
+# Make the directory, then Change directory, then get .pdf file from the web
+mkdir -p /private/graphhopper-data/berlin/
+cd /private/graphhopper-data/berlin/
 wget http://download.geofabrik.de/north-america/us/north-carolina-latest.osm.pbf
 
 echo "Showing contents of ${GRAPHHOPPER_DIR}"
