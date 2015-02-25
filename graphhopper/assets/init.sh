740d06716a146ee2570e56df6bcfac8897ed48b6
Dustin Williams <dusty_williams825@live.com>
2015-02-25 01:08:54 -0500

    3

--- graphhopper/assets/init.sh
+++ graphhopper/assets/init.sh
@@ -1,6 +1,8 @@
 
 #!/bin/sh
 
+wget https://s3.amazonaws.com/metro-extracts.mapzen.com/charlotte_north-carolina.osm.pbf
+
 # Create variable for Graph Hopper Directory
 GRAPHHOPPER_DIR=/graphhopper
 
@@ -12,9 +14,6 @@ cd $GRAPHHOPPER_DIR
 wget https://oss.sonatype.org/content/groups/public/com/graphhopper/graphhopper-web/0.3/graphhopper-web-0.3-bin.zip
 unzip *.zip
 rm *.zip
-
-cd ~/data/
-wget https://s3.amazonaws.com/metro-extracts.mapzen.com/charlotte_north-carolina.osm.pbf
 
 echo "Showing contents of ${GRAPHHOPPER_DIR}"
 ls $GRAPHHOPPER_DIR
